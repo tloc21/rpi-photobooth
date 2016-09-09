06faab47bc91df3699057742e46120a2773ff33b
tloc21 <wtburke@asu.edu>
2016-09-08 20:08:36 -0700

    Happy path works

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ class PhotoBoothForm(BoxLayout):
     def __init__(self, **kwargs):
         super(PhotoBoothForm, self).__init__(**kwargs)
         self.timer_counter = 5
-        self.iteration_counter = 0
+        self.iteration_counter = 1
         start_button = ObjectProperty()
 
     def button_click(self):
@@ -21,35 +21,39 @@ class PhotoBoothForm(BoxLayout):
         Clock.schedule_once(self.disable_start_button,0)
         Clock.schedule_interval(self.take_photos,1)
 
-
     def disable_start_button(self,*args):
         self.start_button.text = 'PROCESSING...'
         self.start_button.disabled = True
 
-    def take_photos(self,*args):
-        self.photo_popup.popup_title = 'Picture ' + str(self.iteration_counter +1 ) + ' of 4'
-        self.photo_popup.popup_label = str(self.timer_counter)
-        if self.timer_counter == 0:
-            self.photo_popup.popup_label = 'POSE'
-            Clock.unschedule(self.take_photos)
-            try:
-                subprocess.check_output("gphoto2 --capture-image-and-download --filename \
-                                /home/wtb/photobooth_images/photobooth%H%M%S.jpg", stderr=subprocess.STDOUT, shell=True)
-            except subprocess.CalledProcessError as e:
-                print e.returncode, e.output
+    def enable_start_button(self, *args):
+        self.start_button.text = 'Touch here to start'
+        self.start_button.disabled = False
 
-            #if 'ERROR' not in gpout:
-            #    self.iteration_counter +=1
-            #elif '-52: \'Could not find the requested device on the USB port\'' in gpout:
-            #    print 'Could not detect USB camera'
-            #    Clock.schedule_once(self.__init__())
-            self.timer_counter = 6
-            if self.iteration_counter < 4:
+    def take_photos(self,*args):
+        self.photo_popup.popup_title = 'Picture ' + str(self.iteration_counter ) + ' of 4'
+        self.photo_popup.popup_label = str(self.timer_counter)
+        if self.timer_counter == 1:
+            self.photo_popup.popup_label = 'POSE'
+        elif self.timer_counter==0:
+            Clock.unschedule(self.take_photos)
+
+            gpout = subprocess.check_output("gphoto2 --capture-image-and-download --filename \
+                                            /home/wtb/photobooth_images/temp_photo" + str(self.iteration_counter)
+                                            + ".jpg", stderr=subprocess.STDOUT, shell=True)
+            if 'ERROR' not in gpout:
+                self.iteration_counter += 1
+
+            self.timer_counter = 4
+
+            if self.iteration_counter < 5:
                 Clock.schedule_interval(self.take_photos, 1)
+            #    self.iteration_counter += 1
             else:
-                self.iteration_counter = 0
-                self.timer_counter = 6
+                self.iteration_counter = 1
+                self.timer_counter = 5
                 self.photo_popup.dismiss()
+                Clock.schedule_once(self.enable_start_button,5)
+
         self.timer_counter -= 1
 
     def take_photo(self):
